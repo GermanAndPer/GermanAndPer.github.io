3c3627067a58937cbc72aebd5c3ce28ed4c325a9
GermAndPer <germanandresperez1@gmail.com>
2023-05-20 20:54:55 -0600

    Se agregan evanecencia de imagenes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-  });
-  
+});
+
+
+window.addEventListener("load", function() {
+  var image = document.querySelector(".fade-in");
+  image.classList.add("visible");
+});
